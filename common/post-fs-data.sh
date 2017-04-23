204fe888a313eb01f3b84d0e9d4304653edc6994
Andrew Leech <andrew@alelec.net>
2017-04-23 23:30:04 +0000

    Update post-fs-data.sh to disable settings that should be handled automatically by magisk hide

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,8 +6,8 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
-/data/magisk/resetprop ro.boot.verifiedbootstate green
-/data/magisk/resetprop ro.boot.veritymode enforcing
+#/data/magisk/resetprop ro.boot.verifiedbootstate green
+#/data/magisk/resetprop ro.boot.veritymode enforcing
 
 # https://forum.xda-developers.com/showpost.php?p=70831797&postcount=3665
 /data/magisk/resetprop ro.build.description "gemini-user 6.0.1 MXB48T V8.1.2.0.MAAMIDI release-keys"
@@ -15,13 +15,13 @@ MODDIR=${0%/*}
 
 /data/magisk/resetprop ro.bootimage.build.fingerprint "Xiaomi/gemini/gemini:6.0.1/MXB48T/V8.1.2.0.MAAMIDI:user/release-keys"
 
-/data/magisk/resetprop selinux.reload_policy 1
+#/data/magisk/resetprop selinux.reload_policy 1
 
 # https://www.reddit.com/r/pokemongodev/comments/58gnq3/root_bypass_the_new_update_leads_repost/
 # /data/magisk/resetprop ro.build.selinux 0
 
-# https://forum.xda-developers.com/showpost.php?p=71154562&postcount=4
-/data/magisk/resetprop ro.debuggable 0
-/data/magisk/resetprop ro.secure 1
-/data/magisk/resetprop ro.build.type user
-/data/magisk/resetprop ro.build.tags release-keys
+## https://forum.xda-developers.com/showpost.php?p=71154562&postcount=4
+#/data/magisk/resetprop ro.debuggable 0
+#/data/magisk/resetprop ro.secure 1
+#/data/magisk/resetprop ro.build.type user
+#/data/magisk/resetprop ro.build.tags release-keys
