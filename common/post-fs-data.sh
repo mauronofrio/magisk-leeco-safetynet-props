25abeb8643d8d8fcf171ba3221959bd837889c04
Andrew Leech <andrew@alelec.net>
2017-04-25 02:38:53 +0000

    Update post-fs-data.sh in preparation of magisk v13 where resetprop has been moved to a function of /data/magisk/magisk

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,25 +6,31 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
-#/data/magisk/resetprop ro.boot.verifiedbootstate green
-#/data/magisk/resetprop ro.boot.veritymode enforcing
+# Unified daemon > v12 moves resetprop to a function of /data/magisk/magisk
+resetprop=/data/magisk/resetprop
+if [ ! -f $resetprop ]; then
+    resetprop="/data/magisk/magisk resetprop"
+fi
+
+#$resetprop ro.boot.verifiedbootstate green
+#$resetprop ro.boot.veritymode enforcing
 
 # https://forum.xda-developers.com/showpost.php?p=70831797&postcount=3665
-/data/magisk/resetprop ro.build.description "gemini-user 6.0.1 MXB48T V8.1.2.0.MAAMIDI release-keys"
-/data/magisk/resetprop ro.build.fingerprint "Xiaomi/gemini/gemini:6.0.1/MXB48T/V8.1.2.0.MAAMIDI:user/release-keys"
+$resetprop ro.build.description "gemini-user 6.0.1 MXB48T V8.1.2.0.MAAMIDI release-keys"
+$resetprop ro.build.fingerprint "Xiaomi/gemini/gemini:6.0.1/MXB48T/V8.1.2.0.MAAMIDI:user/release-keys"
 
-/data/magisk/resetprop ro.bootimage.build.fingerprint "Xiaomi/gemini/gemini:6.0.1/MXB48T/V8.1.2.0.MAAMIDI:user/release-keys"
+$resetprop ro.bootimage.build.fingerprint "Xiaomi/gemini/gemini:6.0.1/MXB48T/V8.1.2.0.MAAMIDI:user/release-keys"
 
-#/data/magisk/resetprop selinux.reload_policy 1
+#$resetprop selinux.reload_policy 1
 
 # https://www.reddit.com/r/pokemongodev/comments/58gnq3/root_bypass_the_new_update_leads_repost/
-# /data/magisk/resetprop ro.build.selinux 0
+# $resetprop ro.build.selinux 0
 
 ## https://forum.xda-developers.com/showpost.php?p=71154562&postcount=4
-#/data/magisk/resetprop ro.debuggable 0
-#/data/magisk/resetprop ro.secure 1
-#/data/magisk/resetprop ro.build.type user
-#/data/magisk/resetprop ro.build.tags release-keys
+#$resetprop ro.debuggable 0
+#$resetprop ro.secure 1
+#$resetprop ro.build.type user
+#$resetprop ro.build.tags release-keys
 
 # https://github.com/topjohnwu/MagiskManager/issues/112#issuecomment-280993699
 sh /magisk/.core/magiskhide/enable
